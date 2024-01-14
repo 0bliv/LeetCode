4f2f6cc511dbdaeb4bd0cf5622bb265109fad719
0bliv <88675619+0bliv@users.noreply.github.com>
2024-01-13 23:25:19 -0300

    Update Valid Palindromev2.py

--- String/Easy/Valid Palindromev2.py
+++ String/Easy/Valid Palindromev2.py
@@ -1,7 +1,4 @@
-class Solution(object):
+class Solution:
     def isPalindrome(self, s):
-        # Preprocess the string: convert to lowercase and remove non-alphanumeric characters
         processed_s = ''.join(char.lower() for char in s if char.isalnum())
-
-        # Check if the processed string is equal to its reversed form
         return processed_s == processed_s[::-1]
